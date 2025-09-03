1cf9681eaf13f2cb0a47be6d2bbb0b4a69d1b372
sirasagi62 <go.sirasagi62+git@gmail.com>
2025-09-03 12:13:24 +0900

    Add component

---
+++ src/chunking/boundary-aware-chunking.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright (c) ushirononeko 2025
+ * Copyright (c) sirasagi62 2025
+ * Published under MIT License
+ * see https://opensource.org/licenses/MIT
+ *
+ * This code was originally created by ushirononeko and modified by sirasagi62
+ * Original: https://github.com/ushironoko/gistdex
+ */
+
+
+
+/**
+ * Boundary-aware chunking for maintaining semantic boundaries in content
+ */
+
+export interface BoundaryChunkOptions {
+  maxChunkSize: number;
+  overlap: number;
+}
+
+export interface BoundaryInfo {
+  type: string;
+  level?: number;
+  name?: string;
+  title?: string;
+}
+
+export interface BoundaryChunk {
+  content: string;
+  startOffset: number;
+  endOffset: number;
+  boundary: BoundaryInfo;
+}
