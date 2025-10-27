fbcc3a82420cac2c4b8eb1cd19a1adc5aa393eb1
Gota Okabe <go.sirasagi62@gmail.com>
2025-10-27 21:08:53 +0900

    Add export Point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export type { BoundaryChunk, Point } from './chunking/boundary-aware-chunking.ts'
+export type { LanguageEnum } from './chunking/language-node-types.ts'
 export { createParserFactory } from './chunking/parser-factory.ts'
 export type { ParserFactory } from './chunking/parser-factory.ts'
 export * from './io/file-operations.ts'
-export type { LanguageEnum } from './chunking/language-node-types.ts'
-export type { BoundaryChunk } from './chunking/boundary-aware-chunking.ts'
 
